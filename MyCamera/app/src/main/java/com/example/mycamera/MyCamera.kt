e7b65d66dec7f940f082a34f18bb751509ccd9e5
Christopher B. S. <117885718+ChristBernardS@users.noreply.github.com>
2025-03-31 21:18:26 +0700

    Update and rename MainActivity.kt to MyCamera.kt

---
+++ MyCamera/app/src/main/java/com/example/mycamera/MyCamera.kt
@@ -0,0 +1,11 @@
+package com.example.mycamera
+
+import android.app.Application
+import com.google.firebase.FirebaseApp
+
+class MyCamera : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        FirebaseApp.initializeApp(this)
+    }
+}
